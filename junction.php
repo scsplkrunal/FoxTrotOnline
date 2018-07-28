69b012664c3dc1f8c73d9cd6e9acfcf437d6b9a6
Alon <alonbenarieh@gmail.com>
2018-07-28 18:19:56 +0300

    added my files to the repository

---
+++ junction.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+try{
+	require_once 'backstage.php';
+	//	require_once 'html_fragments.php';
+
+	if(isset($_POST['class'])){
+
+		if(!defined('DB_USERNAME') && !defined('DB_PASS') && !defined('DB_NAME')){
+			db_choose($_POST);
+		}
+
+		db_connect(); //open DB connection
+
+		if($_POST['class'] != 'no_class'){ // check if needed to create an object or not
+			$obj = new $_POST['class']($_POST); //create object
+			echo json_encode($obj->$_POST['func']($_POST)); //execute the function sent to the server
+		} else{
+			echo json_encode($_POST['func']($_POST)); //execute the function sent to the server
+		}
+
+		$GLOBALS['db_conn']->close(); //close DB connection
+	}
+
+} catch(Exception $e){
+	$json_obj                = new json_obj();
+	$json_obj->error_message = $e->getMessage();
+	$json_obj->error_level   = $e->getCode();
+	$json_obj->status        = false;
+	echo json_encode($json_obj);
+}
