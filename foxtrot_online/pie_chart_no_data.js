d4e3ec9e6b33a2a9b8b76ee7b566a90be15787bc
Alon <alonbenarieh@gmail.com>
2018-07-31 15:51:28 +0300

    Improve pie chart creation

--- foxtrot_online/pie_chart_no_data.js
+++ foxtrot_online/pie_chart_no_data.js
@@ -3,15 +3,7 @@ var chart_id = document.currentScript.getAttribute( 'chart_id' ); //Sent as a pa
 var ctx = $( '#' + chart_id );
 var pie_chart = new Chart( ctx, {
 	type: 'pie',
-	data: {
-		// labels: [], //Will be filled out from the server
-		// datasets: [{
-		// 	data: [], //Will be filled out from the server
-		// 	backgroundColor: [], //Will be filled out from the server
-		// 	borderColor: 'rgb(255,255,255)',
-		// 	borderWidth: 1
-		// }]
-	},
+	data: {}, //Will be filled out through PHP
 	options: {
 		legend: {
 			position: 'right',
