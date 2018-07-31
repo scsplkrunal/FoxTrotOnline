50467bbc8125d8e1fb8e230b2d8eca295a16367d
Alon <alonbenarieh@gmail.com>
2018-07-31 21:08:00 +0300

    Function - drill down on pie chart basis. On click send the data to the server.

--- foxtrot_online/pie_chart_no_data.js
+++ foxtrot_online/pie_chart_no_data.js
@@ -19,7 +19,35 @@ var pie_chart = new Chart( ctx, {
 } );
 
 
-// ctx.onclick = function( evt ){
+/**
+ * If clicking the chart, sent the data to the server.
+ */
+$(document).ready(
+	function() {
+		$( '#' + chart_id )[0].onclick = function(evt) {
+			var activePoints = pie_chart.getElementsAtEvent(evt);
+			if (activePoints[0]) {
+				var chartData = activePoints[0]['_chart'].config.data;
+				var idx = activePoints[0]['_index'];
+
+				var label = chartData.labels[idx];
+				var value = chartData.datasets[0].data[idx];
+				var color = chartData.datasets[0].backgroundColor[idx];
+
+				$.post( "junction.php", {func: 'drill_down_pie_chart', class: 'no_class', label: label, value: value, color: color}, function( data ){
+					
+				});
+
+
+				var output_string = label + ' ' + value + ' ' + color ;
+				alert(output_string);
+			}
+		};
+	}
+);
+
+
+// $( '#' + chart_id ).click(function(  ){
 // 	var activePoints = myNewChart.getElementsAtEvent( evt );
 // 	if( activePoints[0] ){
 // 		var chartData = activePoints[0]['_chart'].config.data;
@@ -32,4 +60,4 @@ var pie_chart = new Chart( ctx, {
 // 		console.log( url );
 // 		alert( url );
 // 	}
-// };
+// });
