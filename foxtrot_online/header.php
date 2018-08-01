df2be62bed1dfbbcc38818d3d22cb1cfaf96cf96
Alon <alonbenarieh@gmail.com>
2018-08-01 16:13:57 +0300

    Log in bug fix

--- foxtrot_online/header.php
+++ foxtrot_online/header.php
@@ -4,11 +4,9 @@ require_once "backstage.php";
 require_once "html_fragments.php";
 
 if(permrep::is_remembered()){
-	if(basename($_SERVER["PHP_SELF"], '.php') == 'login'){ //If URL page is login
+	if(basename($_SERVER["PHP_SELF"], '.php') == 'login'){ //If page is login
 		header("Location: dashboard.php"); //redirect to dashboard
 	}
-}else{
-	if(basename($_SERVER["PHP_SELF"], '.php') != 'login'){
-		header("Location: login.php");
-	}
+} elseif(!isset($_SESSION['permrep_obj']) && basename($_SERVER["PHP_SELF"], '.php') != 'login'){
+	header("Location: login.php"); //redirect to dashboard
 }
