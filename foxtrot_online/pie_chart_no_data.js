3787535877f2751916452cc08d2952fe37f823b2
Alon <alonbenarieh@gmail.com>
2018-08-07 15:45:50 +0300

    % in pie chart tooltip

--- foxtrot_online/pie_chart_no_data.js
+++ foxtrot_online/pie_chart_no_data.js
@@ -14,6 +14,21 @@ var config = {
 					beginAtZero: true
 				}
 			}]
+		},
+		tooltips: {
+			callbacks: {
+				label: function(tooltipItem, data) {
+					var dataset = data.datasets[tooltipItem.datasetIndex];
+					var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+					var total = meta.total;
+					var currentValue = dataset.data[tooltipItem.index];
+					var percentage = parseFloat((currentValue/total*100).toFixed(1));
+					return currentValue + ' (' + percentage + '%)';
+				},
+				title: function(tooltipItem, data) {
+					return data.labels[tooltipItem[0].index];
+				}
+			}
 		}
 	}
 };
