59f3e7e7f04859b95b43e9d7c12714b974a6ec23
Alon <alonbenarieh@gmail.com>
2018-08-13 23:12:55 +0300

    Line chart Javascript modify

--- line_chart_no_data.js
+++ line_chart_no_data.js
@@ -3,17 +3,7 @@ var chart_id = document.currentScript.getAttribute('chart_id'); //Sent as a para
 var ctx = $('#'+chart_id);
 var line_chart = new Chart(ctx, {
 	type: 'line',
-	data: {
-		labels: [], //Will be filled out from the server
-		datasets: [{
-			data: [], //Will be filled out from the server
-			lineTension: 0,
-			backgroundColor: 'transparent',
-			borderColor: '#007bff',
-			borderWidth: 4,
-			pointBackgroundColor: '#007bff'
-		}]
-	},
+	data: {}, //filled up from server
 	options: {
 		scales: {
 			yAxes: [{
