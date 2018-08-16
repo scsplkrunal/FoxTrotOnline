376a0a2f7420ad52325457dfa4ea026c5d647f77
Alon <alonbenarieh@gmail.com>
2018-08-16 16:46:41 +0300

    Add radio buttons to reports form

--- reports.php
+++ reports.php
@@ -29,7 +29,7 @@ require_once 'header.php';
 					<div class="alert" role="alert"></div>
 				</div>
 				<label>Date:</label>
-				<select id="time_periods_select" name="time_period">
+				<select id="time_periods_select" name="time_period" class="mr-2">
 					<option value="all_dates">All Dates</option>
 					<option value="Year to Date">Year to Date</option>
 					<option value="Month to Date">Month to Date</option>
@@ -38,6 +38,34 @@ require_once 'header.php';
 					<option value="Last Month">Last Month</option>
 					<option id="dates_form_option_custom" value="Custom">Custom</option>
 				</select>
+				<div class="row">
+					<div class="col-xs-2 mr-3">
+						<div class="custom-control custom-radio custom-control">
+							<input type="radio" id="trade_date_radio" name="choose_date_radio"
+							       class="custom-control-input">
+							<label class="custom-control-label" for="trade_date_radio">Trade Date</label>
+						</div>
+						<div class="custom-control custom-radio custom-control">
+							<input type="radio" id="date_posted_radio" name="choose_date_radio"
+							       class="custom-control-input">
+							<label class="custom-control-label" for="date_posted_radio">Date
+								Posted</label>
+						</div>
+					</div>
+					<div class="col-xs-3">
+						<div class="custom-control custom-radio custom-control">
+							<input type="radio" id="gross_comm_radio" name="choose_pay_radio"
+							       class="custom-control-input">
+							<label class="custom-control-label" for="gross_comm_radio">Gross
+								Commission</label>
+						</div>
+						<div class="custom-control custom-radio custom-control">
+							<input type="radio" id="net_pay_radio" name="choose_pay_radio"
+							       class="custom-control-input">
+							<label class="custom-control-label" for="net_pay_radio">Net Pay</label>
+						</div>
+					</div>
+				</div>
 				<div class="hidden_form_div">
 					<label>From</label>
 					<input type="date" name="from_date" required><br class="d-xs-block d-sm-none">
@@ -58,7 +86,8 @@ require_once 'header.php';
 						</script>";
 					?>
 					<canvas id="reports_line_chart"></canvas>
-					<script type="text/javascript" src="line_chart_no_data.js" chart_id="reports_line_chart"></script>
+					<script type="text/javascript" src="line_chart_no_data.js"
+					        chart_id="reports_line_chart"></script>
 					<script type="text/javascript">
 						line_chart.data = line_chart_data;
 						line_chart.update();
@@ -69,14 +98,15 @@ require_once 'header.php';
 			<div class="row mt-5 mb-5">
 				<div class="col-lg-6"> <!-- Pie Chart div -->
 					<?php
-					$json_obj = pie_chart_data_and_labels('reports_pie_chart');
+					$json_obj       = pie_chart_data_and_labels('reports_pie_chart');
 					$pie_chart_data = $json_obj->data_arr['pie_chart_data'];
 					echo "<script type='text/javascript'>
 							var pie_chart_data = $pie_chart_data;
 						</script>";
 					?>
 					<canvas id="reports_pie_chart"></canvas>
-					<script type="text/javascript" src="pie_chart_no_data.js" chart_id="reports_pie_chart"></script>
+					<script type="text/javascript" src="pie_chart_no_data.js"
+					        chart_id="reports_pie_chart"></script>
 					<script type="text/javascript">
 						pie_chart.data = pie_chart_data;
 						pie_chart.update();
@@ -85,7 +115,8 @@ require_once 'header.php';
 			</div>
 
 			<!-- Modal -->
-			<div class="modal fade" id="drill_down_pie_chart_modal" tabindex="-1" role="dialog" aria-hidden="true">
+			<div class="modal fade" id="drill_down_pie_chart_modal" tabindex="-1" role="dialog"
+			     aria-hidden="true">
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
