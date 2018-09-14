51bee67ff49b281c876beb601557d33e2d1e5d8f
Alon <alonbenarieh@gmail.com>
2018-09-14 22:52:13 +0300

    Add google analitycs tag

--- backstage.php
+++ backstage.php
@@ -5,6 +5,17 @@ session_start();
  * The head tag of each html document.
  */
 define('HEAD', '
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125789539-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag("js", new Date());
+	
+	gtag("config", "UA-125789539-1");
+	</script>
+	
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
